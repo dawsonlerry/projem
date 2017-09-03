cd624eda03a11877a8946042dd9401b0a400aeb2
dawsonlerry <31598635+dawsonlerry@users.noreply.github.com>
2017-09-04 00:08:50 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace WindowsFormsApp70
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(260, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(300, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(342, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(93, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Oto Sure";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(376, 208);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(139, 95);
+            this.listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(544, 208);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(139, 95);
+            this.listBox2.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Troll King",
+            "Talos",
+            "Snake-Queen",
+            "Harpy-Queen",
+            "Samma",
+            "Deruvish Founder",
+            "Shaula"});
+            this.comboBox1.Location = new System.Drawing.Point(101, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(108, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(461, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(461, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(323, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Location = new System.Drawing.Point(281, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = ":";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(461, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 50);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Manuel Sure";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(56, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(180, 34);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Bossun Çıkacağı Saati \r\nBoss İsmine Göre Seçin\r\n";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(14, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 48);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Alanları Temizle";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(411, 89);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Saat:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(400, 114);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Dakika:";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightCoral;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(453, 40);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 36);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Bilgi:Kullanım";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.OrangeRed;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(79, 66);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 36);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Bilgi:Kullanım";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(372, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(288, 17);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Bossun Kaç Saat sonra Çıkacığını girin\r\n";
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.SteelBlue;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(119, 226);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(98, 48);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "Yeniden Baslat";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(86, 280);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(14, 114);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Boss Seçin >";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(748, 310);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Boss Cıkış Suresi Hesaplama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp70
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int saat, dakika, saniye;
+        void temizle()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            label1.Text = "0";
+            label2.Text = "0";
+            saniye = 0;
+            dakika = 0;
+            saniye = 0;
+            timer1.Enabled = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Hide();
+        }
+    
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int kesilensaat, kesilendadika;
+            kesilensaat = Convert.ToInt32(textBox1.Text);
+            listBox1.Items.Add(kesilensaat);
+            kesilendadika = Convert.ToInt32(textBox2.Text);
+            listBox2.Items.Add(kesilendadika);
+            timer1.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult bilgi = MessageBox.Show("Saati başına 0 koymadan sadece tek sayı olarak giriniz, nokta veya başka birşey kullanmayınız", "BILGI KULLANIM!", MessageBoxButtons.OKCancel);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult bilgi = MessageBox.Show("TK,TALOS,SQ SAATLERI 8 SAAT OLARAK AYARLANMISTIR", "BILGI KULLANIM!", MessageBoxButtons.OKCancel);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Herhangi Bi Boss ismi Seçmediniz!", "HATA");
+            }
+            else
+            {
+                timer1.Enabled = true;
+                MessageBox.Show("ALARMI KURDUNUZ SAAT GELDIGINDE ALARM CALACAKTIR", "BASARILI");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = saat.ToString();
+            label2.Text = dakika.ToString();
+            label3.Text = saniye.ToString();
+            saniye++;
+
+
+            if (saniye >= 60)
+            {
+                dakika++;
+                saniye = 0;
+
+            }
+            if (dakika == 60)
+            {
+                dakika = 0;
+                saat++;
+            }
+            if(comboBox1.Text == "Troll King" && label1.Text == "8")
+            {
+                timer1.Enabled = false;
+                DialogResult mesaj = MessageBox.Show("Boss Saati geldi", "ALARM", MessageBoxButtons.OKCancel);
+                axWindowsMediaPlayer1.URL = @"C:\Users\Steady\Documents\Visual Studio 2017\Projects\WindowsFormsApp70\WindowsFormsApp70\bin\Debug\muzik\1.mp3";
+
+
+            }
+            if (comboBox1.Text == "Talos" && label1.Text == "8")
+            {
+                timer1.Enabled = false;
+                axWindowsMediaPlayer1.URL = @"C:\Users\Steady\Documents\Visual Studio 2017\Projects\WindowsFormsApp70\WindowsFormsApp70\bin\Debug\muzik\1.mp3";
+                DialogResult mesaj = MessageBox.Show("Boss Saati geldi", "ALARM", MessageBoxButtons.OKCancel);
+                if(mesaj == DialogResult.OK)
+                {
+                    axWindowsMediaPlayer1.URL = "";
+                }
+             
+            }
+            if (comboBox1.Text == "Snake-Queen" && label2.Text == "1")
+            {
+                timer1.Enabled = false;
+                axWindowsMediaPlayer1.URL = @"C:\Users\Steady\Documents\Visual Studio 2017\Projects\WindowsFormsApp70\WindowsFormsApp70\bin\Debug\muzik\1.mp3";
+                DialogResult mesaj = MessageBox.Show("Boss Saati geldi", "ALARM", MessageBoxButtons.OKCancel);
+               
+            }
+            if (comboBox1.Text == "Harpy-Queen" && label1.Text == "6")
+            {
+                timer1.Enabled = false;
+                axWindowsMediaPlayer1.URL = @"C:\Users\Steady\Documents\Visual Studio 2017\Projects\WindowsFormsApp70\WindowsFormsApp70\bin\Debug\muzik\1.mp3";
+                DialogResult mesaj = MessageBox.Show("Boss Saati geldi", "ALARM", MessageBoxButtons.OKCancel);
+              
+            }
+            if (comboBox1.Text == "Samma" && label1.Text == "6")
+            {
+                timer1.Enabled = false;
+                axWindowsMediaPlayer1.URL = @"C:\Users\Steady\Documents\Visual Studio 2017\Projects\WindowsFormsApp70\WindowsFormsApp70\bin\Debug\muzik\1.mp3";
+                DialogResult mesaj = MessageBox.Show("Boss Saati geldi", "ALARM", MessageBoxButtons.OKCancel);
+             
+            }
+            if (comboBox1.Text == "Deruvish Founder" && label2.Text == "6")
+            {
+                timer1.Enabled = false;
+                axWindowsMediaPlayer1.URL = @"C:\Users\Steady\Documents\Visual Studio 2017\Projects\WindowsFormsApp70\WindowsFormsApp70\bin\Debug\muzik\1.mp3";
+                DialogResult mesaj = MessageBox.Show("Boss Saati geldi", "ALARM", MessageBoxButtons.OKCancel);
+               
+            }
+            if (comboBox1.Text == "Shaula" && label1.Text == "6")
+            {
+                timer1.Enabled = false;
+                axWindowsMediaPlayer1.URL = @"C:\Users\Steady\Documents\Visual Studio 2017\Projects\WindowsFormsApp70\WindowsFormsApp70\bin\Debug\muzik\1.mp3";
+                DialogResult mesaj = MessageBox.Show("Boss Saati geldi", "ALARM", MessageBoxButtons.OKCancel);
+                
+            }
+
+            if (label1.Text == textBox1.Text && label2.Text == textBox2.Text)
+            {
+                timer1.Enabled = false;
+                axWindowsMediaPlayer1.URL = @"C:\Users\Steady\Documents\Visual Studio 2017\Projects\WindowsFormsApp70\WindowsFormsApp70\bin\Debug\muzik\1.mp3";
+                DialogResult mesaj = MessageBox.Show("Boss Saati geldi", "ALARM", MessageBoxButtons.OKCancel);
+                if (mesaj == DialogResult.OK)
+                {
+                    axWindowsMediaPlayer1.URL = "";
+                }
+
+            }
+            {
+
+            }
+        }
+    }
+}
